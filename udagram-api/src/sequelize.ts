c1d94ed8eb547a681838a197de342980d5c747e1
oasic <j@oasic.net>
2025-02-05 17:48:26 -0500

    Fix bugs in Udacity code; Functioning monolith

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -10,4 +10,10 @@ export const sequelize = new Sequelize({
 
   'dialect': config.dialect,
   'storage': ':memory:',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Change to `true` if using a verified CA certificate
+    },
+  },
 });
